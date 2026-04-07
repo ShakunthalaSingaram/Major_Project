12843d1b05669ca1fd901b603ad9448eb1773ef3
Shakunthala Singaram <2472499@cognizant.com>
2026-04-07 10:39:02 +0530

    final  Commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,15 +13,12 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
   /* Retry failed tests one time */
   retries: 1,
-  //retries: process.env.CI ? 2 : 0,
-
   /* Opt out of parallel tests on CI. */
   workers: 1,
   // workers: process.env.CI ? 1 : undefined, 
